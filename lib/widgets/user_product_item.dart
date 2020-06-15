9d06fed774654e98196425d802e9cf0bea2cb845
AhmedAbogameel <gamilco81@gmail.com>
2020-06-16 01:37:34 +0200

    shop app

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shopapp/screens/edit_product_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:shopapp/providers/products_provider.dart';
 
 class UserProductItem extends StatelessWidget {
-  UserProductItem(this.id,this.title,this.imageUrl);
+  UserProductItem(this.id, this.title, this.imageUrl);
 
   final String id;
   final String title;
@@ -12,17 +13,35 @@ class UserProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     return ListTile(
       title: Text(title),
-      leading: CircleAvatar(backgroundImage: NetworkImage(imageUrl),),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
       trailing: Container(
         width: 100,
         child: Row(
           children: <Widget>[
-            IconButton(icon: Icon(Icons.edit), color: Theme.of(context).primaryColor , onPressed: (){Navigator.pushNamed(context, EditProductScreen.routeName,arguments: id);}),
-            IconButton(icon: Icon(Icons.delete), color: Theme.of(context).errorColor, onPressed: (){
-              Provider.of<ProductsProvider>(context,listen: false).deleteProduct(id);
-            }),
+            IconButton(
+                icon: Icon(Icons.edit),
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  Navigator.pushNamed(context, EditProductScreen.routeName,
+                      arguments: id);
+                }),
+            IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () async {
+                  try {
+                    await Provider.of<ProductsProvider>(context, listen: false)
+                        .deleteProduct(id);
+                  } catch (e) {
+                    scaffold.showSnackBar(
+                        SnackBar(content: Text('Deleting failed!',textAlign: TextAlign.center,)));
+                  }
+                }),
           ],
         ),
       ),
