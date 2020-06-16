5eb05dd6fa6553ddaa4f159b2f3e63bef78e4a98
AhmedAbogameel <gamilco81@gmail.com>
2020-06-16 02:21:43 +0200

    shop app

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier{
   Product({
@@ -17,9 +20,26 @@ class Product with ChangeNotifier{
   final String imageUrl;
   bool isFavorite;
 
-  void toggleFavoriteStatus(){
+  void _setFavValue(bool newValue){
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  void toggleFavoriteStatus()async{
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = 'https://shop-app-dcff6.firebaseio.com/products/$id.json';
+    try{
+      final response = await http.patch(url,body: json.encode({
+        'isFavorite':isFavorite,
+      }));
+      if(response.statusCode >= 400){
+        _setFavValue(oldStatus);
+      }
+    }catch(e){
+      _setFavValue(oldStatus);
+    }
   }
 
 }
